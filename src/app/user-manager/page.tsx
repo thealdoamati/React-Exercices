c70b3d1d1b852aa1de39d64c9c41ea3b0c527028
Aldo Amati <112902097+thealdoamati@users.noreply.github.com>
2024-11-19 23:48:24 -0300

    upd: Filtrando tabela

--- src/app/user-manager/page.tsx
+++ src/app/user-manager/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { useState } from 'react'
+
 const pendingUsers = [
   {
     id: 1,
@@ -42,10 +45,68 @@ const pendingUsers = [
 ]
 
 export default function UserManager() {
+  const [filterDatesTo, setFilterDatesTo] = useState('')
+  const [orderedPendingUsers, setOrderedPendingUsers] = useState(pendingUsers)
+
+  function handleFilterDates() {
+    if (filterDatesTo === 'todayToPast') {
+      setOrderedPendingUsers(
+        // Eu coloco o ... pois assim eu uso a última forma do pendingUsers para fazer o sort
+        [...pendingUsers].sort(
+          (a, b) =>
+            new Date(a.requestDate).getTime() -
+            new Date(b.requestDate).getTime(),
+        ),
+      )
+      setFilterDatesTo('pastToToday')
+    } else {
+      setOrderedPendingUsers(
+        [...pendingUsers].sort(
+          (a, b) =>
+            new Date(b.requestDate).getTime() -
+            new Date(a.requestDate).getTime(),
+        ),
+      )
+      setFilterDatesTo('todayToPast')
+    }
+  }
+
+  console.log('filterDatesTo', filterDatesTo)
+  console.log('orderedPendingUsers', orderedPendingUsers)
+
   return (
-    <div>
+    <div className="flex flex-col p-4 gap-4">
+      <button
+        className="flex gap-2 border p-1 rounded-md w-fit"
+        onClick={handleFilterDates}
+      >
+        Filtrar{' '}
+        {filterDatesTo === 'todayToPast' ? '(Mais novos)' : '(Mais antigos)'}
+      </button>
+
       <table>
-        <tbody></tbody>
+        <thead>
+          <th>id</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Request Date</th>
+          <th>Status</th>
+          <th>Company</th>
+        </thead>
+        <tbody>
+          {orderedPendingUsers.map((user) => {
+            return (
+              <tr className="border-b-1 border" key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.requestDate}</td>
+                <td>{user.status}</td>
+                <td>{user.company}</td>
+              </tr>
+            )
+          })}
+        </tbody>
       </table>
     </div>
   )
--- src/components/NPS/index.tsx
+++ src/components/NPS/index.tsx
@@ -1,67 +1,66 @@
 'use client'
-import { ChangeEvent, FormEvent, useState } from "react"
+import { ChangeEvent, FormEvent, useState } from 'react'
 
-const numbers = Array.from({length: 11}, (_,i) => i)
-
+const numbers = Array.from({ length: 11 }, (_, i) => i)
 
 export default function NPSComponent() {
-    const [NPSnumber, setNPSnumber] = useState('')
+  const [NPSnumber, setNPSnumber] = useState('')
 
-    function handleSelectedNumber(event: ChangeEvent<HTMLInputElement>) {
-        setNPSnumber(event.target.value)
-        console.log(event.target.value)
+  function handleSelectedNumber(event: ChangeEvent<HTMLInputElement>) {
+    setNPSnumber(event.target.value)
+    console.log(event.target.value)
+  }
 
+  function getColor(number: number) {
+    if (number >= 10) {
+      return 'bg-green-300 border-green-300'
+    } else if (number >= 7) {
+      return 'bg-yellow-300 border-yellow-300'
+    } else {
+      return 'bg-red-300 border-red-300'
     }
+  }
 
-    function getColor(number: number) {
-        if(number >= 10) {
-            return 'bg-green-300 border-green-300'
-        } else if( number >= 7) {
-            return 'bg-yellow-300 border-yellow-300'
-        } else {
-            return 'bg-red-300 border-red-300'
-        }   
-    }
+  function handleSendForm(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    console.log('Nota selecionada:', NPSnumber)
+  }
 
-    function handleSendForm(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault()
-        console.log('Nota selecionada:', NPSnumber)
-    }
-    
-
-    return(
-        <>
-        <h3>O quanto você recomendaria este produto?</h3>
-            <form onSubmit={handleSendForm}>
-                <div className="flex gap-6">
-                {numbers.map(number => {
-                    return(
-                        <div 
-                        onClick={() => setNPSnumber(number.toString())} // Adicionei onClick para melhor interatividade
-                        className={`
+  return (
+    <>
+      <h3>O quanto você recomendaria este produto?</h3>
+      <form onSubmit={handleSendForm}>
+        <div className="flex gap-6">
+          {numbers.map((number) => {
+            return (
+              <div
+                onClick={() => setNPSnumber(number.toString())} // Adicionei onClick para melhor interatividade
+                className={`
                             flex flex-col items-center p-2 rounded border-2
-                            ${NPSnumber === number.toString() 
-                                ? getColor(number) 
-                                : 'bg-gray-50 border-gray-200'}
+                            ${
+                              NPSnumber === number.toString()
+                                ? getColor(number)
+                                : 'bg-gray-50 border-gray-200'
+                            }
                             hover:bg-gray-100 cursor-pointer transition-all
-                        `}  
-                        key={number}
-                    >                        
-                        <input 
-                            className="hidden" 
-                            value={number} 
-                            type="radio" 
-                            name="NPS" 
-                            checked={NPSnumber === number.toString()} 
-                            onChange={handleSelectedNumber} 
-                        />
-                        <label className="cursor-pointer font-medium">{number}</label>
-                    </div>
-                    )
-                })}
-                </div>
-                <input type="submit" value="Enviar" />
-            </form>
-        </>
-    )
+                        `}
+                key={number}
+              >
+                <input
+                  className="hidden"
+                  value={number}
+                  type="radio"
+                  name="NPS"
+                  checked={NPSnumber === number.toString()}
+                  onChange={handleSelectedNumber}
+                />
+                <label className="cursor-pointer font-medium">{number}</label>
+              </div>
+            )
+          })}
+        </div>
+        <input type="submit" value="Enviar" />
+      </form>
+    </>
+  )
 }
